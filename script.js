64b177675c9380856e38a3eb26474f7ad33340a0
Charles <ckelly218@gmail.com>
2022-09-11 14:46:19 -0700

    Add winning combinations

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
+// Get array from cell divs
 let cells = document.querySelectorAll('.cell');
 cells = Array.from(cells)
+let playerOne = "X";
 
-let currentPlayer = "X";
+// winning combinations
+let winningCombinations = [
+    [0,1,2],
+    [3,4,5],
+    [6.7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
 
-cells.forEach(cell =>{
-    cell.addEventListener('click',()=>{
-        cell.innerText ="X"
 
+// Alternate click input for X and O
+cells.forEach(cell => {
+    cell.addEventListener('click', () => {
+        if(cell.innerText.trim() != "") return
+        cell.innerText = playerOne
+        playerOne = playerOne === "X" ? "O" : "X"
     })
 })
+
