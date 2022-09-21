3b37fce27fbe808bedbdde28bf005b24c95f7b02
Charles <ckelly218@gmail.com>
2022-09-20 21:36:46 -0700

    Add win/draw condition. Add endGame function and board reset

--- script.js
+++ script.js
@@ -1,27 +1,85 @@
-// Get array from cell divs
-let cells = document.querySelectorAll('.cell');
-cells = Array.from(cells)
-let playerOne = "X";
+// Get cells
 
-// winning combinations
-let winningCombinations = [
+let cells = document.querySelectorAll('.cell');
+let cellArray = Array.from(cells)
+
+
+const resetButton = document.getElementById('reset');
+const winningMessage = document.getElementById('winner-text');
+let currentTurn = "X";
+
+resetButton.addEventListener('click',resetBoard);
+
+const winningCombinations = [
+    // rows
     [0,1,2],
     [3,4,5],
-    [6.7,8],
+    [6,7,8],
+    // columns
     [0,3,6],
     [1,4,7],
     [2,5,8],
+    // diagnoal
     [0,4,8],
     [2,4,6]
-]
+];
+
+gameStart()
+
+function gameStart(){
+    cellArray.forEach(cell => cell.addEventListener('click',handleClick, {once:true}));
+
+}
+
+function handleClick(e){
+    const cellIndex = this.getAttribute("cell")
+    console.log(this)
+    // const cell = e.target;
+    // console.log(cell)
+    this.innerText = currentTurn;
+    // checkForWinner(currentTurn)
+    // currentTurn = currentTurn === "X" ? "O" : "X";
+    checkWinner()
+    switchTurns()
+    checkDraw()
+}
 
 
-// Alternate click input for X and O
-cells.forEach(cell => {
-    cell.addEventListener('click', () => {
-        if(cell.innerText.trim() != "") return
-        cell.innerText = playerOne
-        playerOne = playerOne === "X" ? "O" : "X"
+function switchTurns() {
+    currentTurn = currentTurn === "X" ? "O" : "X";
+}
+
+
+function checkWinner(){
+    winningCombinations.forEach(combination => {
+        let winner = combination.every(combo => cellArray[combo].innerText === currentTurn)
+        if(winner){
+            winningMessage.innerText = `${currentTurn} wins!`
+            endGame()
+        }else{
+
+        }             
     })
-})
+}
+
+function endGame(){
+    cellArray.forEach(cell => cell.removeEventListener('click',handleClick, {once:true}));
+}
+
+function resetBoard(){
+    window.location.reload(true)
+}
+
+function checkDraw(){
+    let tie = cellArray.every(cell => cell.innerText != "")
+    if(tie){
+        winningMessage.innerText = "It's a tie!"
+    }
+}
+
+
+
+// cellArray.forEach(cell => {
+//     console.log(cell.innerText != "")
+// })
 
